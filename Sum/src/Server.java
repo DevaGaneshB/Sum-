908cd247142155a57229197e02c3c37ce8384b3b
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-08-13 11:34:22 +0530

    Add files via upload

---
+++ Sum/src/Server.java
@@ -0,0 +1,17 @@
+
+import java.io.*;  
+import java.net.*;  
+public class Server {  
+public static void main(String[] args){  
+try{  
+ServerSocket ss=new ServerSocket(6666);  
+Socket s=ss.accept();//establishes connection   
+DataInputStream dis=new DataInputStream(s.getInputStream());  
+String  str=(String)dis.readUTF();  
+String  a=(String)dis.readUTF();  
+String  b=(String)dis.readUTF();  
+System.out.println("Sum= "+(Integer.parseInt(a)+Integer.parseInt(b)));  
+ss.close();  
+}catch(Exception e){System.out.println(e);}  
+}  
+}  
